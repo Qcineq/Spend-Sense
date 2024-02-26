40bd28f42edf77dcc61c8b1da9df8a10ba60b2bb
Kuba <jakub.kucinski@icloud.com>
2024-02-26 14:33:29 +0100

    Initial Commit

---
+++ Spend Sense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spend Sense
+//
+//  Created by Jakub Kuciński on 26/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spend Sense/Spend_SenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_SenseApp.swift
+//  Spend Sense
+//
+//  Created by Jakub Kuciński on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spend_SenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
